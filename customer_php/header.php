364047232059dbc231f8102e78e2c7773efb97ca
Sandeep-177 <sandeepkumarpradhan177@gmail.com>
2025-09-08 08:07:28 +0530

    header.php links path set

--- customer_php/header.php
+++ customer_php/header.php
@@ -1,18 +1,18 @@
     <!-- Header -->
     <header>
         <div class="logo">Smart Home<span style="color: brown;">Service</span></div>
-        <div id="barImg"><a id="barImg2" href="#"><img src="/code/icon/bars-solid.svg" alt="no bar image"></a></div>
+        <div id="barImg"><a id="barImg2" href="#"><img src="icon/bars-solid.svg" alt="no bar image"></a></div>
         <nav id="headerNav">
             <ul id="abAfterblink">
                         <li class="afterLnk"><a href="/code/index.php" class="naviLink" data-target="0">Home</a>
                         </li>
-                        <li class="afterLnk"><a href="/code/customer_php/category.php" class="naviLink"
+                        <li class="afterLnk"><a href="customer_php/category.php" class="naviLink"
                                 data-target="1">Services</a></li>
-                        <li class="afterLnk"><a href="/code/customer_php/contactUs.php" class="naviLink"
+                        <li class="afterLnk"><a href="customer_php/contactUs.php" class="naviLink"
                                 data-target="2">Contact</a></li>
-                        <li class="afterLnk"><a href="/code/customer_php/feedback.php" class="naviLink"
+                        <li class="afterLnk"><a href="customer_php/feedback.php" class="naviLink"
                                 data-target="3">Feedback</a></li>
                     </ul>
         </nav>
     </header>
-    <script src="/code/customer_php/smScrnNav.js" defer></script>
+    <script src="customer_php/smScrnNav.js" defer></script>
