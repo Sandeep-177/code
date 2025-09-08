9c21e9b0a68a404161ebfdccacbec02e11e3ef34
Sandeep-177 <sandeepkumarpradhan177@gmail.com>
2025-09-08 19:17:11 +0530

    customer_php header link path edit

--- customer_php/header.php
+++ customer_php/header.php
@@ -4,13 +4,13 @@
         <div id="barImg"><a id="barImg2" href="#"><img src="../icon/bars-solid.svg" alt="no bar image"></a></div>
         <nav id="headerNav">
             <ul id="abAfterblink">
-                        <li class="afterLnk"><a href="../index.php" class="naviLink" data-target="0">Home</a>
+                        <li class="afterLnk"><a href="/index.php" class="naviLink" data-target="0">Home</a>
                         </li>
-                        <li class="afterLnk"><a href="customer_php/category.php" class="naviLink"
+                        <li class="afterLnk"><a href="/customer_php/category.php" class="naviLink"
                                 data-target="1">Services</a></li>
-                        <li class="afterLnk"><a href="customer_php/contactUs.php" class="naviLink"
+                        <li class="afterLnk"><a href="/customer_php/contactUs.php" class="naviLink"
                                 data-target="2">Contact</a></li>
-                        <li class="afterLnk"><a href="customer_php/feedback.php" class="naviLink"
+                        <li class="afterLnk"><a href="/customer_php/feedback.php" class="naviLink"
                                 data-target="3">Feedback</a></li>
                     </ul>
         </nav>
