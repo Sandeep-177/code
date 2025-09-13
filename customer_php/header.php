306de823c596809339a09073c1664f80f28d846f
Sandeep-177 <sandeepkumarpradhan177@gmail.com>
2025-09-13 09:10:24 +0530

    header link modifying

--- customer_php/header.php
+++ customer_php/header.php
@@ -8,13 +8,13 @@
                         </li>
                         <li class="afterLnk"><a href="index.php" class="naviLink" data-target="0">Home</a>
                         </li>
-                        <li class="afterLnk"><a href="customer_php/category.php" class="naviLink"
+                        <li class="afterLnk"><a href="/customer_php/category.php" class="naviLink"
                                 data-target="1">Services</a></li>
-                        <li class="afterLnk"><a href="customer_php/contactUs.php" class="naviLink"
+                        <li class="afterLnk"><a href="/customer_php/contactUs.php" class="naviLink"
                                 data-target="2">Contact</a></li>
-                        <li class="afterLnk"><a href="customer_php/feedback.php" class="naviLink"
+                        <li class="afterLnk"><a href="/customer_php/feedback.php" class="naviLink"
                                 data-target="3">Feedback</a></li>
                     </ul>
         </nav>
     </header>
-    <script src="customer_php/smScrnNav.js" defer></script>
+    <script src="/customer_php/smScrnNav.js" defer></script>
